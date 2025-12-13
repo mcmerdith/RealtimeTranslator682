ef41ffcf0f83b0f7e869f134851655a03bda6b31
Matthew Meredith <matthewmeredith775@gmail.com>
2025-12-12 21:31:50 -0500

    add helper functions, error handling

--- app/src/main/java/com/cisc682/realtimetranslator/lib/TranslationLib.kt
+++ app/src/main/java/com/cisc682/realtimetranslator/lib/TranslationLib.kt
@@ -4,13 +4,19 @@ import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import kotlinx.coroutines.tasks.await
+import java.util.Locale
 
 class TranslationLib {
     companion object {
-        suspend fun translate(sourceLanguage: String, targetLanguage: String, text: String): String {
+        suspend fun translate(sourceLangTag: String, targetLangTag: String, text: String): String {
+            val sourceLang = TranslateLanguage.fromLanguageTag(sourceLangTag)
+            val targetLang = TranslateLanguage.fromLanguageTag(targetLangTag)
+            if (sourceLang == null || targetLang == null) {
+                return "Translation failed (not supported)!"
+            }
             val options = TranslatorOptions.Builder()
-                .setSourceLanguage(TranslateLanguage.fromLanguageTag(sourceLanguage)!!)
-                .setTargetLanguage(TranslateLanguage.fromLanguageTag(targetLanguage)!!)
+                .setSourceLanguage(sourceLang)
+                .setTargetLanguage(targetLang)
                 .build()
             val translator = Translation.getClient(options)
             try {
@@ -19,6 +25,18 @@ class TranslationLib {
             } finally {
                 translator.close()
             }
+        }
+
+        fun getLocale(languageTag: String): Locale {
+            return Locale.forLanguageTag(languageTag)
+        }
+
+        fun getDisplayName(languageTag: String): String {
+            return getLocale(languageTag).displayLanguage
+        }
+
+        fun getAllLanguages(): List<String> {
+            return TranslateLanguage.getAllLanguages()
         }
     }
 }
