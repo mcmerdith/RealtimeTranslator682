83ca37f832507ddaa7ef40226148819e49c2f64d
Matthew Meredith <matthewmeredith775@gmail.com>
2025-12-14 05:06:21 -0500

    refactor conversations page to be more modular, reliable, and well documented

--- app/src/main/java/com/cisc682/realtimetranslator/pages/Conversation.kt
+++ app/src/main/java/com/cisc682/realtimetranslator/pages/Conversation.kt
@@ -1,5 +1,6 @@
 package com.cisc682.realtimetranslator.pages
 
+import android.Manifest.permission_group.PHONE
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,13 +25,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cisc682.realtimetranslator.lib.TranslationLib
 import com.cisc682.realtimetranslator.ui.components.LanguageDropdown
@@ -44,56 +45,36 @@ import kotlinx.coroutines.launch
 data class Message(val primaryText: String, val secondaryText: String, val isPrimary: Boolean)
 
 @OptIn(ExperimentalPermissionsApi::class)
-@PreviewScreenSizes
+@Preview(device = PHONE)
 @Composable
 fun ConversationPage() {
-    // Languages
-    var primaryLangTag by remember { mutableStateOf("en") }
-    var secondaryLangTag by remember { mutableStateOf("es") }
-
-    // Information about conversation state
-    var isSwapped by remember { mutableStateOf(false) }
-    var conversationHistory by remember { mutableStateOf(listOf<Message>()) }
-
-
-    val onSwapLanguages = {
-        isSwapped = !isSwapped
-    }
-
-    val onPrimaryLanguageChange = { newLanguage: String ->
-        primaryLangTag = newLanguage
-    }
-
-    val onSecondaryLanguageChange = { newLanguage: String ->
-        secondaryLangTag = newLanguage
-    }
-
-    // Speech recognition state
-    var isPrimarySpeaker by remember { mutableStateOf(true) }
     val coroutineScope = rememberCoroutineScope()
 
-    // Speech to Text
-    val showSpeechRecognition = createSpeechRecognizer(
-        language = if (isPrimarySpeaker) primaryLangTag else secondaryLangTag,
-        flipped = if (isSwapped) isPrimarySpeaker else !isPrimarySpeaker,
-        onSpeechResult = {
-            coroutineScope.launch {
-                val sourceLang = if (isPrimarySpeaker) primaryLangTag else secondaryLangTag
-                val targetLang = if (isPrimarySpeaker) secondaryLangTag else primaryLangTag
-                val translatedText = TranslationLib.translate(sourceLang, targetLang, it)
-                conversationHistory = conversationHistory + if (isPrimarySpeaker) Message(
-                    it, translatedText, true
-                ) else Message(translatedText, it, false)
-            }
-        })
+    // Languages
+    var primaryLangTag by rememberSaveable { mutableStateOf("en") }
+    var secondaryLangTag by rememberSaveable { mutableStateOf("es") }
 
-    val onMicClick = { isPrimary: Boolean ->
-        isPrimarySpeaker = isPrimary
-        showSpeechRecognition()
+    // Information about conversation state
+    var isSwapped by rememberSaveable { mutableStateOf(false) }
+    var conversationHistory by rememberSaveable { mutableStateOf(listOf<Message>()) }
+
+    val swapLanguages = { isSwapped = !isSwapped }
+
+    val translateAndAddMessage = { isPrimary: Boolean, text: String ->
+        // Translate and store state
+        coroutineScope.launch {
+            val sourceLang = if (isPrimary) primaryLangTag else secondaryLangTag
+            val targetLang = if (isPrimary) secondaryLangTag else primaryLangTag
+            val translatedText = TranslationLib.translate(sourceLang, targetLang, text)
+            conversationHistory = conversationHistory + if (isPrimary) Message(
+                text, translatedText, true
+            ) else Message(translatedText, text, false)
+        }
     }
 
     Column(
         modifier = Modifier.fillMaxSize().let {
+            // Rotate the entire page if swapped
             if (isSwapped) {
                 it.graphicsLayer { rotationZ = 180f }
             } else {
@@ -107,11 +88,14 @@ fun ConversationPage() {
                 .graphicsLayer { rotationZ = 180f }) {
             ConversationHalf(
                 languageTag = secondaryLangTag,
-                messages = conversationHistory,
-                onSwap = onSwapLanguages,
                 isPrimary = false,
-                onMicClick = { onMicClick(false) },
-                onLanguageChange = onSecondaryLanguageChange
+                isSwapped = isSwapped,
+                messages = conversationHistory,
+                onSwapPressed = swapLanguages,
+                onLanguageChange = { newLanguage: String -> secondaryLangTag = newLanguage },
+                onMessageCreated = {
+                    translateAndAddMessage(false, it)
+                }
             )
         }
         Column(
@@ -121,28 +105,51 @@ fun ConversationPage() {
         ) {
             ConversationHalf(
                 languageTag = primaryLangTag,
-                messages = conversationHistory,
-                onSwap = onSwapLanguages,
                 isPrimary = true,
-                onMicClick = { onMicClick(true) },
-                onLanguageChange = onPrimaryLanguageChange
+                isSwapped = isSwapped,
+                messages = conversationHistory,
+                onSwapPressed = swapLanguages,
+                onLanguageChange = { newLanguage: String -> primaryLangTag = newLanguage },
+                onMessageCreated = {
+                    translateAndAddMessage(true, it)
+                }
             )
         }
     }
 }
 
+/**
+ * The primary user interface for the conversation view
+ *
+ * @param languageTag The current language of this half
+ * @param isPrimary Whether this half is the primary speaker
+ * @param isSwapped Whether the speakers positions are swapped
+ * @param messages The conversation history
+ * @param onSwapPressed The function to call when the swap button is pressed
+ * @param onLanguageChange The function to call when the language dropdown is changed
+ * @param onMessageCreated The function to call when a message is created
+ */
 @Composable
 fun ConversationHalf(
     languageTag: String,
-    messages: List<Message>,
-    onSwap: () -> Unit,
     isPrimary: Boolean,
-    onMicClick: () -> Unit,
-    onLanguageChange: (String) -> Unit
+    isSwapped: Boolean,
+    messages: List<Message>,
+    onSwapPressed: () -> Unit,
+    onLanguageChange: (String) -> Unit,
+    onMessageCreated: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    // Text to Speech
     val speak = createTextToSpeech(languageTag)
+    // Speech to Text
+    val showSpeechRecognition = createSpeechRecognizer(
+        languageTag = languageTag,
+        flipped = if (isSwapped) isPrimary else !isPrimary,
+        onSpeechResult = { onMessageCreated(it) })
+
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -165,6 +172,7 @@ fun ConversationHalf(
                             start = 8.dp, end = 8.dp, top = 8.dp, bottom = 72.dp
                         ), verticalArrangement = Arrangement.spacedBy(8.dp), reverseLayout = true
                     ) {
+                        // Messages are stored as "latest last" but we want latest first
                         items(messages.reversed()) { message ->
                             val isMyMessage = message.isPrimary == isPrimary
                             val bubbleColor =
@@ -199,12 +207,12 @@ fun ConversationHalf(
                     }
                 }
                 IconButton(
-                    onClick = onMicClick,
+                    onClick = showSpeechRecognition,
                     modifier = Modifier
                         .align(Alignment.BottomCenter)
                         .padding(16.dp)
                 ) {
-                    Icon(Icons.Default.Mic, contentDescription = "Record audio")
+                    Icon(Icons.Default.Mic, contentDescription = "Speak message")
                 }
             }
         }
@@ -218,11 +226,53 @@ fun ConversationHalf(
         ) {
             LanguageDropdown(languageTag, onLanguageChange)
             Spacer(modifier = Modifier.weight(1f))
-            Button(onClick = onSwap) {
+            Button(onClick = onSwapPressed) {
                 Icon(Icons.Default.SwapHoriz, contentDescription = "Swap Languages")
                 Spacer(modifier = Modifier.width(4.dp))
                 Text(text = "Swap")
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun ConversationHalfPreview() {
+    val messages = listOf(
+        Message("Hello!", "Hola!", true),
+        Message("How are you?", "Cómo estás?", false),
+        Message("I'm fine, thank you.", "Bien, gracias.", true),
+        Message(
+            "do you know where there is a library nearby",
+            "¿Sabes dónde hay una biblioteca cerca?",
+            false
+        )
+    )
+    Column(modifier = Modifier.fillMaxSize()) {
+        ConversationHalf(
+            "es",
+            isPrimary = false,
+            isSwapped = false,
+            messages = messages,
+            onSwapPressed = {},
+            onLanguageChange = {},
+            onMessageCreated = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        )
+        ConversationHalf(
+            "en",
+            isPrimary = true,
+            isSwapped = false,
+            messages = messages,
+            onSwapPressed = {},
+            onLanguageChange = {},
+            onMessageCreated = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        )
+
     }
 }
